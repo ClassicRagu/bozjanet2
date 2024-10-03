af1295895a382aff7442451649e73a8e5f88eb86
ClassicRagu <megameowthethird@gmail.com>
2024-10-03 00:14:23 -0500

    Update relics guide

--- src/components/relic/onetimestep2/OneTimeStep2.tsx
+++ src/components/relic/onetimestep2/OneTimeStep2.tsx
@@ -57,21 +57,40 @@ function OneTimeStep2() {
           Each of these quests requires 30 of 2 items. Each item correlates to
           Skirmishes and Critical Engagements in each zone of Zadnor. This is
           simply completed by doing those Skirmishes and Critical Engagements.
+          Though you may find that you are lacking the CE items in particular.
           <br />
           <br />
-          As a note, the alternatives for this step are almost never worth
-          doing, even the CE ones as they require specific raids to be completed
-          synced. If you are having issues with the CE items in particular I
-          suggest spawning them yourself, just follow the cluster farming
-          section{" "}
-          <a href="https://docs.google.com/presentation/d/1ax8nkureepnIoTTSdcNhyIHQgCLqbcZzoe_H8kpyunU/edit?usp=sharing">
-            in this guide
-          </a>{" "}
-          and kill 4th legion magitek in each zone to spawn CES. You can keep
-          track of what CEs have spawned so far{" "}
-          <a href="https://syldrathecat.github.io/xivcat/bozja-tracker/#zadnor">
-            on this site.
-          </a>
+          The alternative raids are almost never worth it for the Skirmish items
+          but it&apos;s a completely different story for the CE items. Due to
+          the frequency of CEs spawning being quite low on average, it can be a
+          better use of your time to do the raids. If you end up completing the
+          Skirmish items and still need CE ones, you can queue for the following
+          raids:
+          <ul>
+            <li>
+              Spare Parts:{" "}
+              <a href="https://ffxiv.consolegameswiki.com/wiki/Alexander_-_The_Burden_of_the_Father">
+                Alexander The Burden of the Father
+              </a>
+            </li>
+            <li>
+              Tell Me a Story:{" "}
+              <a href="https://ffxiv.consolegameswiki.com/wiki/Deltascape_V3.0">
+                Deltascape V3.0
+              </a>
+            </li>
+            <li>
+              A Fond Memory:{" "}
+              <a href="https://ffxiv.consolegameswiki.com/wiki/Eden%27s_Gate:_Inundation">
+                Eden&apos;s Gate: Inundation
+              </a>
+            </li>
+          </ul>
+          It should be noted, some discord servers, like{" "}
+          <a href="https://discord.gg/agentsofeureka">Agents of Eureka</a> on
+          NA, occasionally host cluster farms in Zadnor. These farms tend to
+          spawn almost all of the CEs in the instance so it may be worth trying
+          to join the same instance as those farming to complete the CE items.
         </AccordionDetails>
       </Accordion>
     </>
--- src/components/relic/step3/Step3.tsx
+++ src/components/relic/step3/Step3.tsx
@@ -35,44 +35,56 @@ function RelicStep3() {
           <strong>Step 3: The Will to Resist</strong>
         </AccordionSummary>
         <AccordionDetails>
-          Step three requires you to obtain 6{" "}
-          <a href="https://ffxiv.consolegameswiki.com/wiki/Bitter_Memory_of_the_Dying">
-            Bitter Memory of the Dying
-          </a>
-          <br />
-          <br />
-          <u>This is actually the exact same as step two so skip this if you have
-          already read it.</u>
-          <br />
-          <br />
-          There are multiple ways to obtain these but the quickest way to do
-          every Bozja relic step is to actually do them in Bozja itself.
-          <br />
-          <br />
-          The fastest way to get these memories is to join an omnifarm. These
-          farms are hosted on average 1-2 times a month, usually over on CEM
-          listed in our NA server list on the{" "}
-          <Link href="/">main guide page.</Link>
-          <br />
-          Omnifarms give on average 4-5 weapons worth of memories making them
-          easily the best way to obtain them in the game.
-          <br />
-          <br />
-          If you don&apos;t want to wait for an omnifarm, it&apos;s best to form a cluster
-          and/or fragment farm party in BSF or see about organzing one in a
-          discord server like <a href="https://discord.gg/foexiv">FOE</a> as
-          they tend to still be as fast as alternative methods like Blue Mage
-          while giving you additional items like{" "}
-          <a href="https://ffxiv.consolegameswiki.com/wiki/Bozjan_Cluster">
-            Bozjan Clusters
-          </a>{" "}
-          in the process.
-          <br />
-          <br />
+          <p>
+            Step three requires you to obtain 6{" "}
+            <a href="https://ffxiv.consolegameswiki.com/wiki/Bitter_Memory_of_the_Dying">
+              Bitter Memory of the Dying
+            </a>
+          </p>
+          <p>
+            <u>
+              This is step almost idenical to step 2. I&apos;ve
+              underlined some key differences for those who have already read
+              that step.
+            </u>
+          </p>
+          <p>
+            There are multiple ways to obtain these but the quickest way to do
+            every Bozja relic step is to actually do them in Bozja itself.
+          </p>
+          <p>
+            The fastest way to get these memories is to join an omnifarm. These
+            farms are hosted on average 1-2 times a month, usually over on CEM
+            listed in our NA server list on the{" "}
+            <Link href="/">main guide page.</Link>{" "}
+            <u>
+              As an important note, a single omnifarm will likely get you enough
+              memories for every single relic weapon.
+            </u>
+          </p>
+          <p>
+            If you don&apos;t want to wait for an omnifarm, it&apos;s best to
+            form a cluster and/or fragment farm party in BSF or see about
+            organzing one in a discord server like{" "}
+            <a href="https://discord.gg/foexiv">FOE</a> as they tend to still be
+            as fast as alternative methods like Blue Mage while giving you
+            additional items like{" "}
+            <a href="https://ffxiv.consolegameswiki.com/wiki/Bozjan_Cluster">
+              Bozjan Clusters
+            </a>{" "}
+            in the process.
+          </p>
           <a href="https://docs.google.com/presentation/d/1ax8nkureepnIoTTSdcNhyIHQgCLqbcZzoe_H8kpyunU/edit">
             For more information on fragment and cluster farming please feel
             free to look at this guide
           </a>
+          <p>
+            <u>
+              Though I heavily suggest omnifarms for this step, it can also be
+              done relatively passively if you are leveling as you get 1 for
+              doing Leveling Roulette daily.
+            </u>
+          </p>
         </AccordionDetails>
       </Accordion>
     </>
