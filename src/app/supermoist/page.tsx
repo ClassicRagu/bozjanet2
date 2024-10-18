9f196b332ba412ef5db18f3bb599dfe501ba90d3
ClassicRagu <megameowthethird@gmail.com>
2024-10-17 23:56:58 -0500

    Even more cleanup

--- src/app/supermoist/page.tsx
+++ src/app/supermoist/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 import EurekaFarmGuide from "@/components/supermoist/EurekaFarmGuide";
 import FarmSelector from "@/components/supermoist/FarmSelector";
+import FarmSliders from "@/components/supermoist/FarmSliders";
 import WeatherTable from "@/components/supermoist/WeatherTable";
 import { getWindows } from "@/functions/weather/getWindows";
-import { marks, snow } from "@/static/weather/SliderMarks";
 import { FarmInfo } from "@/types/weather/FarmInfo";
 import { WindowTimes } from "@/types/weather/WindowTimes";
-import {
-  Box,
-  Card, Grid2, Modal, Slider,
-  Typography
-} from "@mui/material";
+import { Box, Card, Modal } from "@mui/material";
 import React from "react";
 
 function Weather() {
@@ -101,66 +97,12 @@ function Weather() {
                 src="/relic/step1/ScalepowderLocation.PNG"
               />
             </Modal>
-            <Grid2
-              container
-              spacing={2}
-              style={{
-                minHeight: "75px",
-                maxWidth: "1000px",
-                marginBottom: "10px",
-                alignItems: "top",
-                justifyContent: "center",
-                width: "100%",
-                marginTop: "10px",
-              }}
-            >
-              <Grid2
-                style={{
-                  alignContent: "center",
-                  justifyContent: "center",
-                  maxHeight: "50px",
-                  minWidth: "250px",
-                }}
-              >
-                <Typography gutterBottom>Weeks</Typography>
-                <Slider
-                  style={{ width: "90%", maxWidth: "600px" }}
-                  aria-label="Weeks"
-                  value={weekState}
-                  onChange={(e, newValue) => {
-                    if (typeof newValue == "number") setWeekState(newValue);
-                  }}
-                  max={8}
-                  min={1}
-                  step={null}
-                  valueLabelDisplay="off"
-                  marks={marks}
-                />
-              </Grid2>
-              <Grid2
-                style={{
-                  alignContent: "center",
-                  justifyContent: "center",
-                  maxHeight: "50px",
-                  minWidth: "250px",
-                }}
-              >
-                <Typography gutterBottom>Windows</Typography>
-                <Slider
-                  style={{ width: "90%", maxWidth: "600px" }}
-                  aria-label="Weeks"
-                  value={findSnowState}
-                  onChange={(e, newValue) => {
-                    if (typeof newValue == "number") setFindSnowState(newValue);
-                  }}
-                  max={5}
-                  min={1}
-                  step={1}
-                  valueLabelDisplay="off"
-                  marks={snow}
-                />
-              </Grid2>
-            </Grid2>
+            <FarmSliders
+              setWeekState={setWeekState}
+              weekState={weekState}
+              setFindSnowState={setFindSnowState}
+              findSnowState={findSnowState}
+            />
             <div
               style={{
                 justifyContent: "center",
---
+++ src/components/supermoist/FarmSliders.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { marks, snow } from "@/static/weather/SliderMarks";
+import { Grid2, Slider, Typography } from "@mui/material";
+
+type FarmSlidersProps = {
+  setWeekState: React.Dispatch<React.SetStateAction<number>>;
+  weekState: number;
+  setFindSnowState: React.Dispatch<React.SetStateAction<number>>;
+  findSnowState: number;
+};
+
+function FarmSliders(props: FarmSlidersProps) {
+  const {setWeekState, weekState, setFindSnowState, findSnowState} = props
+
+  return (
+    <Grid2
+      container
+      spacing={2}
+      style={{
+        minHeight: "75px",
+        maxWidth: "1000px",
+        marginBottom: "10px",
+        alignItems: "top",
+        justifyContent: "center",
+        width: "100%",
+        marginTop: "10px",
+      }}
+    >
+      <Grid2
+        style={{
+          alignContent: "center",
+          justifyContent: "center",
+          maxHeight: "50px",
+          minWidth: "250px",
+        }}
+      >
+        <Typography gutterBottom>Weeks</Typography>
+        <Slider
+          style={{ width: "90%", maxWidth: "600px" }}
+          aria-label="Weeks"
+          value={weekState}
+          onChange={(e, newValue) => {
+            if (typeof newValue == "number") setWeekState(newValue);
+          }}
+          max={8}
+          min={1}
+          step={null}
+          valueLabelDisplay="off"
+          marks={marks}
+        />
+      </Grid2>
+      <Grid2
+        style={{
+          alignContent: "center",
+          justifyContent: "center",
+          maxHeight: "50px",
+          minWidth: "250px",
+        }}
+      >
+        <Typography gutterBottom>Windows</Typography>
+        <Slider
+          style={{ width: "90%", maxWidth: "600px" }}
+          aria-label="Weeks"
+          value={findSnowState}
+          onChange={(e, newValue) => {
+            if (typeof newValue == "number") setFindSnowState(newValue);
+          }}
+          max={5}
+          min={1}
+          step={1}
+          valueLabelDisplay="off"
+          marks={snow}
+        />
+      </Grid2>
+    </Grid2>
+  );
+}
+
+export default FarmSliders;
